2c2bd6cc1314538291bbb1a4da3f5faa16b9aa47
lndslm <lindasalim.ca@gmail.com>
2023-08-31 19:02:16 -0400

    Built the hero and the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">
+          <div className="hero">
+            <h1>Dictionary</h1>
+          </div>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Open-sourced code by Linda Salim</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function Search(event) {
+    event.preventDefault;
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <div className="Search-engine">
+        <h5>What word are you looking for ?</h5>
+        <form onSubmit={Search}>
+          <input type="search" onChange={handleKeywordChange} />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+    </div>
+  );
+}
