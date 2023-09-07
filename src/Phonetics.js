0d2c56ae8b3b67ee6df12a79d5049c1cd593fc5c
lndslm <lindasalim.ca@gmail.com>
2023-09-07 13:08:26 -0400

    Integrated the React Audio Player component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,12 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
       {props.phonetic.text}
       <br />
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
-        Listen
-      </a>
+      <ReactAudioPlayer src={props.phonetic.audio} controls />
     </div>
   );
 }
