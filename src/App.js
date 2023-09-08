ce1ccf1b86e3044e7f8edb3ccb870030afb2626d
lndslm <lindasalim.ca@gmail.com>
2023-09-07 21:42:05 -0400

    Fixed the footer at the bottom of the screen

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <div className="hero">
-            <h1>Dictionary</h1>
-          </div>
-        </header>
-        <main>
-          <Dictionary />
-        </main>
+        <div>
+          <header className="App-header">
+            <div className="hero">
+              <h1>Dictionary</h1>
+            </div>
+          </header>
+          <main>
+            <Dictionary />
+          </main>
+        </div>
         <footer className="App-footer">
           <a
             href="https://github.com/lndslm/dictionary-react-app"
